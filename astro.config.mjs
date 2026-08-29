970bdd24cd929bda73ab62c50bb404497cad6a43
mcenahle <hi@mcenahle.page>
2026-08-30 06:45:25 +0800

    init: 网站初始化，在 mcenahle.dev 的基础上进行修改而来

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://mcenahle.care",
+  trailingSlash: "always",
+  prefetch: true,
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [icon()],
+});
